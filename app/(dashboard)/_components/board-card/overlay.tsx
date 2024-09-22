3e54a8b4eec709299507c2994be4a811403e6681
viraj padale <vpadale86@gmail.com>
2024-09-23 00:13:26 +0530

    board list-home screen completed

---
+++ app/(dashboard)/_components/board-card/overlay.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Overlay = () => {
+  return (
+    <div 
+    className='group-hover:backdrop-blur-md transition-all duration-500 ease-in-out h-full w-full'>
+    </div>
+  )
+}
+
+export default Overlay
--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,90 @@
 "use client"
+
+import Link from "next/link";
+import Image from "next/image";
+import {Poppins} from "next/font/google"
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard ,Star} from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+const font=Poppins({
+    subsets:["latin"],
+    weight:["600"]
+});
 export const OrgSidebar=()=>{
+    const searchParams=useSearchParams()
+    const favorites=searchParams.get("favorites");
 return (
     <div className="hidden lg:flex flex-col space-y-6 
-    w-[206px] pl-5 pt-5 bg-red-500">Org sidebar</div>
+    w-[206px] pl-5 pt-5">
+        <Link href="/">
+            <div className="flex items-center gap-x-2">
+                <Image
+                src="/logo.svg"
+                alt="logo"
+                height={60}
+                width={60}
+                />
+                <span className={cn(
+                    "font-semibold text-xl",
+                    font.className,
+                )}>
+                JAMBOARD
+                </span>
+            </div>
+        </Link>
+        <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+            elements:{
+                rootBox:{
+                    display:"flex",
+                    justifyContent:"center",
+                    alignItems:"center",
+                    width:"100%",
+                },
+                organizationSwitcherTrigger:{
+                    padding:"6px",
+                    width:"100%",
+                    borderRadius:"8px",
+                    border:"1px solid #E5E7EB",
+                    justifyContent:"space-between",
+                    backgroundColor:"white",
+                }
+            }
+        }}
+        />
+        <div className="space-y-1 w-full">
+            <Button
+            variant={favorites?"ghost":"outline"}
+            asChild
+            size="lg"
+            className="font-normal justify-start px-2 w-full"
+            >
+                <Link href="/">
+                <LayoutDashboard className="h-4 w-4 mr-2"/>
+                Team boards
+                </Link>
+            </Button>
+        </div>
+        <div className="space-y-1 w-full">
+            <Button
+            variant={favorites?"outline":"ghost"}
+            asChild
+            size="lg"
+            className="font-normal justify-start px-2 w-full"
+            >
+                <Link href={{
+                    pathname:"/",
+                    query:{favorites:true}
+                }}>
+                <Star className="h-4 w-4 mr-2"/>
+                Favorite boards
+                </Link>
+            </Button>
+        </div>
+    </div>
 )
 }
